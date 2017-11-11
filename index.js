a49ef94753131c4b881528362ce68c1d7c6926f1
Michael Myung <michael.h.myung@gmail.com>
2017-11-11 02:16:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName
+
+function upperCaseCustomerName(customerName) {
+  return toUpperCase(customerName)
+}
+
+function setBestCustomer(bestCustomer) {
+  var bestCustomer = "not bob"
+}
+
+function overWriteBestCustomer(bestCustomer) {
+  var bestCustomer = ""
+}
+
+const leastFavoriteCustomer = "Guy"
+
+function changeLeastFavoriteCustomer(name) {
+  const leastFavoriteCustomer = "other guy"
+}
+
+var favoriteCustomer 
