4375936c456ab587ea5baa87af25ac64ddae11a1
Michael Myung <michael.h.myung@gmail.com>
2017-11-11 02:38:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var customerName = "bob"
 
 function upperCaseCustomerName(customerName) {
-  var customerName.toUpperCase(customerName)
+  var customerName = toUpperCase(customerName)
 }
 
 function setBestCustomer(bestCustomer) {
